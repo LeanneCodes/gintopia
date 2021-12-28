8403a347bcea54a544f391a60ec816b989a755ee
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2021-12-28 16:35:50 +0000

    add google map api key to contact page

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "uL3GpaGhKJ2wOu54K-zozuByAIg";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
