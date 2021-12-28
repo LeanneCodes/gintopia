a12e8c3e85b333923feb10e53d0eb285394496b8
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2021-12-28 19:44:35 +0000

    amend the service and template ID to the emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_d29ijgj","template_lbx3fth", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "gin_booking": contactForm.ginbooking.value
