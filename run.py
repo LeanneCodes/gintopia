fa6040f497a15c94c235de73a4fcc0dc923afa5a
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-01-04 17:58:57 +0000

    add run.py file and flask framework

---
+++ run.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/allaboutgin")
+def allaboutgin():
+    return render_template("allaboutgin.html")
+
+
+@app.route("/bookaclass")
+def bookaclass():
+    return render_template("bookaclass.html")
+
+
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
